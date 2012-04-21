6506a1c779fc3f2ec465523d780d5d44ca2e0d14
Leonardo Antonio Alves <leoaalvs@gmail.com>
2012-04-21 16:43:18 -0300

    Inserindo dois JoptionPanes

--- src/grafico.java
+++ src/grafico.java
@@ -5,6 +5,7 @@ public class grafico {
 
 	public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "Teste em Git");
+		JOptionPane.showMessageDialog(null, "Outra mensagem");
 
 	}
 
